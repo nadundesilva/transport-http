a7bd23fde88135b66b73bff1c01e182597deb515
nadundesilva <nadunrds@gmail.com>
2018-10-08 16:52:36 +0530

    Add netty handlers for vick tracing

--- components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/sender/HttpClientChannelInitializer.java
+++ components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/sender/HttpClientChannelInitializer.java
@@ -53,6 +53,8 @@ import org.wso2.transport.http.netty.sender.http2.ClientFrameListener;
 import org.wso2.transport.http.netty.sender.http2.Http2ClientChannel;
 import org.wso2.transport.http.netty.sender.http2.Http2ConnectionManager;
 import org.wso2.transport.http.netty.sender.http2.Http2TargetHandler;
+import org.wso2.transport.http.netty.vick.HeaderAdjustmentInboundHandler;
+import org.wso2.transport.http.netty.vick.HeaderAdjustmentOutboundHandler;
 
 import javax.net.ssl.SSLException;
 
@@ -207,6 +209,12 @@ public class HttpClientChannelInitializer extends ChannelInitializer<SocketChann
         HttpClientUpgradeHandler upgradeHandler = new HttpClientUpgradeHandler(sourceCodec, upgradeCodec,
                 Integer.MAX_VALUE);
         pipeline.addLast(Constants.HTTP2_UPGRADE_HANDLER, upgradeHandler);
+
+        pipeline.addLast(
+                new HeaderAdjustmentInboundHandler(),
+                new HeaderAdjustmentOutboundHandler()
+        );
+
         pipeline.addLast(Constants.TARGET_HANDLER, targetHandler);
     }
 
@@ -230,6 +238,12 @@ public class HttpClientChannelInitializer extends ChannelInitializer<SocketChann
     public void configureHttpPipeline(ChannelPipeline pipeline, TargetHandler targetHandler) {
         pipeline.addLast(Constants.HTTP_CLIENT_CODEC, new HttpClientCodec());
         addCommonHandlers(pipeline);
+
+        pipeline.addLast(
+                new HeaderAdjustmentInboundHandler(),
+                new HeaderAdjustmentOutboundHandler()
+        );
+
         pipeline.addLast(Constants.TARGET_HANDLER, targetHandler);
     }
 
